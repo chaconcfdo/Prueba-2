ebf9c47c31a2a4c9769a6599b331fa0b6dbd508a
Erick Carpio <31169347+chaconcfdo@users.noreply.github.com>
2017-08-19 16:43:41 -0600

    Index
    Pantalla inicial

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Prueba</title>
+	<link rel="stylesheet" type="text/css" href="infols/css/bootstrap-3.3.7-dist/css/bootstrap.css">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<script src="http://code.jquery.com/jquery-latest.js"></script>
+</head>
+<body>
+	<h2>Prueba</h2>
+	<hr>
+	<h3>Está es una prueba alojada en GitHub</h3>
+	
+</body>
+</html>
